e011351983877f91c8261c32ab744f6a8fc23aca
Ziad Mahmoud <ziadmahmoudx98@gmail.com>
2024-02-19 19:22:05 +0200

    final commit

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import "bulma/css/bulma.css"
 import ProfileCard from "./components/ProfileCard"
 import alexaImgae from "./images/alexa.png"
 import cortanaImage from "./images/cortana.png"
@@ -6,10 +7,23 @@ import siriImage from "./images/siri.png"
 function App() {
   return (
     <>
-      <h1>The assistants</h1>
-      <ProfileCard image={alexaImgae} title="Alexa" handle="@Alexa" description="Alexa is Amazon assistant" />
-      <ProfileCard image={cortanaImage} title="Cortana" handle="@Cortana" description="Cortana is Micorsoft assistant" />
-      <ProfileCard image={siriImage} title="Siri" handle="@Siri" description="Siri is Apple assistant" />
+      <section className="hero is-info mb-6">
+        <div className="hero-body">
+          <p className="title">The assistants</p>
+        </div>
+      </section>
+
+      <div className="columns is-centered">
+        <div className="column is-3">
+          <ProfileCard image={alexaImgae} title="Alexa" handle="@Alexa" description="Alexa is Amazon assistant" />
+        </div>
+        <div className="column is-3">
+          <ProfileCard image={cortanaImage} title="Cortana" handle="@Cortana" description="Cortana is Micorsoft assistant" />
+        </div>
+        <div className="column is-3">
+          <ProfileCard image={siriImage} title="Siri" handle="@Siri" description="Siri is Apple assistant" />
+        </div>
+      </div>
     </>
   )
 }
--- src/components/ProfileCard.js
+++ src/components/ProfileCard.js
@@ -1,10 +1,23 @@
 function ProfileCard({ image, title, handle, description }) {
   return (
-    <div>
-      <img src={image} alt="Assistant Image" />
-      <p>{title}</p>
-      <p>{handle}</p>
-      <p>{description}</p>
+    <div className="card">
+      <div className="card-image">
+        <figure className="image is-4by3">
+          <img src={image} alt="Assistant" />
+        </figure>
+      </div>
+      <div className="card-content">
+        <div className="media">
+          <div className="media-content">
+            <p>{title}</p>
+            <p>{handle}</p>
+          </div>
+        </div>
+
+        <div className="content">
+          <p>{description}</p>
+        </div>
+      </div>
     </div>
   )
 }
